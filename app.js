077cc88c7b4a53d1734717f23a69a5af340ec839
Emanuel Cardozo <ecardozo@digbang.com>
2022-08-05 18:30:02 -0300

    Changing package.json settings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
